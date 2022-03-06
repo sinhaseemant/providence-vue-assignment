6a96ff51392c3c7bbfe293ddeaf7986460e579b8
Seemant Sinha <sinhaseemant@gmail.com>
2022-03-06 22:29:35 +0530

    added CSS and added ADD and REMOVE button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <input type="text" id="username" name="username" v-model="username" placeholder="Username" />
-  <br />
-  <br />
-  <input type="email" id="email" name="email" v-model="email" placeholder="Email"/>
-  <br />
-  <br />
-  <button v-on:click="getData()">Submit</button>
-  <br />
-  <br />
-  <div>
-    <p v-for="user in list" v-bind:key="user" ref="user"> UserName: {{user.username}} <br> Email: {{user.email}}</p> 
+  <div v-for="index in list" v-bind:key="index" class="card">
+    <div>
+    <input
+      type="text"
+      id="username"
+      name="username"
+      v-model="index.username"
+      placeholder="Username"
+    />
+    <input
+      type="email"
+      id="email"
+      name="email"
+      v-model="index.email"
+      placeholder="Email"
+    />
+
+    </div>
+    <button v-on:click="add()">Add</button>
+    <button v-on:click="remove(index)" id="remove">Remove</button>
+
+
+    <!-- <p v-for="user in list" v-bind:key="user" ref="user">
+      UserName: {{ user.username }} <br />
+      Email: {{ user.email }}
+    </p> -->
   </div>
 </template>
 
@@ -18,19 +33,22 @@ export default {
   name: "App",
   data() {
     return {
-      list: [],
-      username: '',
-      email: ''
+      list: [{
+        username: "",
+        email: ""
+      }],
     };
   },
 
   methods: {
-    getData: function () {
+    add: function () {
       this.list.push({
-        username :this.username ,
-        email : this.email
-
-      })
+        username:"" ,
+        email: ""
+      });
+    },
+    remove: function (index) {
+      this.list.splice(this.list.indexOf(index), 1);
     },
   },
 };
@@ -44,5 +62,34 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.card {
+  background-color: #ecf0f1;
+  border-radius: 4px;
+  margin: 0 auto;
+  padding: 0 20px 20px;
+  width: 360px;
+  margin-top: 20px;
+}
+input {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 1em;
+  padding: 10px;
+  margin: 10px 5px;
+  border: 0;
+  border-bottom: 1px solid #757575;
+}
+button {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 1em;
+  padding: 10px;
+  margin: 10px 5px;
+  border: 0;
+  border-radius: 3px;
+  background-color: #3498db;
+  color: #ecf0f1;
+}
+#remove {
+  background-color: #e74c3c;
 }
 </style>
